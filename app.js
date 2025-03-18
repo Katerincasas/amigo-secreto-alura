82ba65b49014633078e78fc7ea6697d1d7636ff8
Katerin Casas <ingkaterincasas@gmail.com>
2025-03-17 22:31:26 -0500

    Primer commit: estructura básica del proyecto y primer array para almacenar nombres

---
+++ app.js
@@ -0,0 +1,3 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// declarando el array de amigos
+let amigos = [];
